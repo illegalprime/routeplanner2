37f17020eb6175c76b7183ec86b7053452690c22
Michael Eden <themichaeleden@gmail.com>
2023-01-12 15:08:21 -0500

    move domains

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :routeplanner, RouteplannerWeb.Endpoint,
-  url: [host: "routeplanner.michaels.toys", scheme: "https", port: 443],
+  url: [host: "routes.gbtu.xyz", scheme: "https", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
